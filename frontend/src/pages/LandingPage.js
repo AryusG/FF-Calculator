9426d249b77a6d1e59620cef3234cfbcaee04666
Jarrah <jarrahalek@gmail.com>
2022-06-27 04:36:58 +1000

    Added google authentication button, handled auth state change in browser

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "../components/NavBar";
+import 'firebase/auth';
+import { getIdToken, getIdTokenResult, GoogleAuthProvider, onAuthStateChanged, signInWithPopup } from "firebase/auth";
 
 function LandingPage() {
-  const handleSignIn = () => {
-    // Do your ting
+  const [auth, setAuth] = useState(false) || window.localStorage.getItem("loggedin") === "true";
+  const [token, setToken] = useState("");
+
+  useEffect(()=> {
+    onAuthStateChanged((userCred) => {
+      if (userCred) 
+        setAuth(true);
+        window.localStorage.setItem("loggedin", "true");
+        getIdToken().then((token) => {
+          setToken(token);
+        })
+    })
+  })
+
+  const googleLogIn = () => {
+    signInWithPopup(new GoogleAuthProvider()).then((userCred) => {
+      if (userCred) {
+        setAuth(true);
+      }
+    });
   };
 
   return (
     <div className="bg-purple-900 w-full h-screen">
-      <NavBar />
+      {auth ? (
+        <NavBar />
+        // landing page shit
+      ) : (
+      <button onClick={googleLogIn}>Login with google</button>
+      )}
     </div>
   );
 }
