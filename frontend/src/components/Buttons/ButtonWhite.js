b287461e783f99a7caed3fc55832e6ec23131824
AryusG <verdyg24@gmail.com>
2022-06-27 01:26:49 +1000

    navbar to be finished

---
+++ frontend/src/components/Buttons/ButtonWhite.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ButtonWhite({ name }) {
+  return (
+    <button className="btn font-ubuntu bg-white text-pink">
+      {name}
+    </button>
+  )
+};
+
+export default ButtonWhite;
