175fe4942a19a09f53974c71d6c81d4b7feabbde
AryusG <verdyg24@gmail.com>
2022-06-30 01:03:42 +1000

    formatted

--- frontend/src/components/PortalComponents/SignUpCard.js
+++ frontend/src/components/PortalComponents/SignUpCard.js
@@ -1,93 +1,107 @@
-import { Link } from 'react-router-dom'
-import GooglePng from '../../assets/sign-in-svgs/Google.png'
-import React, { useState } from 'react'
-import { onAuthStateChanged, signInWithEmailAndPassword, 
-        GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword} from "firebase/auth";
-import {auth} from "../../firebase-config/firebase-config"
-import { MAX_VALUE_MILLIS } from '@firebase/util';
+import { Link } from "react-router-dom";
+import GooglePng from "../../assets/sign-in-svgs/Google.png";
+import React, { useState } from "react";
+import {
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../../firebase-config/firebase-config";
+import { MAX_VALUE_MILLIS } from "@firebase/util";
 
 function SignUpCard() {
-  async function registerWithGoogle(user) {
-  }
-  
+  async function registerWithGoogle(user) {}
+
   async function registerEmail(user) {
     try {
-      const userCredential = await createUserWithEmailAndPassword(auth, user.email, user.password);
-      console.log(userCredential)
-      if (!userCredential) throw "User already exists"
-    }
-    catch (err){
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        user.email,
+        user.password
+      );
+      console.log(userCredential);
+      if (!userCredential) throw "User already exists";
+    } catch (err) {
       console.log(err);
     }
   }
 
   const [user, setUser] = useState({
     email: "",
-    password: ""
+    password: "",
   });
+
+  const [reEnteredPass, setReEnteredPass] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-  }
+    console.log(user);
+    console.log(reEnteredPass);
+  };
 
   return (
-    <div className="flex card-white font-ubuntu sm:ml-4 sm:mr-10 mx-16 px-8
-      max-w-sm">
+    <div
+      className="flex card-white font-ubuntu sm:ml-4 sm:mr-10 mx-16 px-8
+      max-w-sm"
+    >
       <div className="justify-center">
         <div className="flex">
-          <div className='text-3xl font-medium px-7'>
-            Get Started
-          </div>
+          <div className="text-3xl font-medium px-7">Get Started</div>
         </div>
         <form className="flex-col pb-4 pt-7 px-7" onSubmit={handleSubmit}>
           <div id="signup-form-1" className="relative">
-            <input 
-              placeholder="Email *" 
-              className="peer input-gray w-full" 
-              required 
+            <input
+              placeholder="Email *"
+              className="peer input-gray w-full"
+              required
               value={user.email}
-              onChange={(e) => setUser({...user, email: e.target.value})} 
-              />
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+            />
             <label className="absolute floating-label">Email</label>
           </div>
           <div id="signup-form-2" className="relative">
-            <input 
-              placeholder="Password *" 
+            <input
+              placeholder="Password *"
               className="peer input-gray w-full"
               required
               value={user.password}
-              onChange={(e) => setUser({...user, password: e.target.value})}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
             />
-            <label className="absolute floating-label">
-              Password
-            </label>
+            <label className="absolute floating-label">Password</label>
           </div>
           <div id="signup-form-3" className="relative">
-            <input placeholder="Re-enter Password *" className="peer input-gray w-full"/>
+            <input
+              placeholder="Re-enter Password *"
+              className="peer input-gray w-full"
+              required
+              value={reEnteredPass}
+              onChange={(e) => setReEnteredPass(e.target.value)}
+            />
             <label className="absolute floating-label">Re-enter Password</label>
           </div>
           <div className="flex mx-8 pt-2 pb-5 justify-center">
             <div className="content-center sm:mr-4 mr-2">
-              <input type="checkbox"/>
+              <input type="checkbox" />
             </div>
             <p className="text-sm cursor-pointer hover:underline">
               I agree to FF-Land's Privacy Policy
             </p>
-          </div> 
+          </div>
           <div>
-            <button 
-              type="submit"
-              className="btn-pink w-full mt-1">Register</button>
+            <button type="submit" className="btn-pink w-full mt-1">
+              Register
+            </button>
           </div>
         </form>
         <div className="px-7 ">
-          <button className="btn-white border border-pink inline-flex 
-            justify-center items-center w-full px-7 mb-2">
-            <img src={GooglePng} alt="Google SVG" className="h-4 mr-3"/>
-            <span>
-              Register with Google
-            </span>
+          <button
+            className="btn-white border border-pink inline-flex 
+            justify-center items-center w-full px-7 mb-2"
+          >
+            <img src={GooglePng} alt="Google SVG" className="h-4 mr-3" />
+            <span>Register with Google</span>
           </button>
           <div className="font-light text-sm">
             <Link to="/portal/login">
@@ -99,7 +113,7 @@ function SignUpCard() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default SignUpCard
+export default SignUpCard;
