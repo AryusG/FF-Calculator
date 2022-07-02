d6f4b8974e0d8f18f7f8df18d95342e0f18f6637
Jarrah <jarrahalek@gmail.com>
2022-07-03 02:02:21 +1000

    implemented log in and sign up functionality

--- frontend/src/components/PortalComponents/SignUpCard.js
+++ frontend/src/components/PortalComponents/SignUpCard.js
@@ -1,18 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import GooglePng from "../../assets/sign-in-svgs/Google.png";
 import React, { useState } from "react";
+import validator from "validator"
+import {createUserWithEmailAndPassword} from "firebase/auth";
+import { auth } from "../../firebase-config/firebase-config";
 import {
   onAuthStateChanged,
-  signInWithEmailAndPassword,
   GoogleAuthProvider,
   signInWithPopup,
-  createUserWithEmailAndPassword,
 } from "firebase/auth";
-import { auth } from "../../firebase-config/firebase-config";
-import { MAX_VALUE_MILLIS } from "@firebase/util";
+
 
 function SignUpCard() {
-  async function registerWithGoogle(user) {}
+
+  const navigate = useNavigate();
 
   const [user, setUser] = useState({
     email: "",
@@ -20,32 +21,62 @@ function SignUpCard() {
     reEnteredPass: "",
   });
 
+  const [isAuth, setAuth] = useState(false);
+
+
+  async function registerGoogle(user) {
+    const provider = new GoogleAuthProvider();
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+        
+      if (credential)
+        setAuth(true);
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
+
+  async function registerEmail(user) {
+    try {
+      await createUserWithEmailAndPassword(
+      auth,
+      user.email,
+      user.password
+      );
+      setAuth(true);
+    } 
+    catch (err) {
+      console.log(err);
+      alert("Email already in use, log in to FF-Land!")
+    }
+  }
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    async function registerEmail(user) {
-      try {
-        const userCredential = await createUserWithEmailAndPassword(
-          auth,
-          user.email,
-          user.password
-        );
-        console.log(userCredential);
-        if (!userCredential) throw "User already exists";
-      } catch (err) {
-        console.log(err);
-      }
+    if (!validator.isEmail(user.email)) {
+      alert("Email must be valid.");
+      return;
     }
 
-    // Need to check if email input is a legit email
-    if (user.password === user.reEnteredPass) {
-      console.log("passwords match");
-      registerEmail(user);
+    if (user.password !== user.reEnteredPass) {
+      alert("Passwords must match.");
+      return;
     }
-    // Implement if passwords don't match with re-entered
+
+    if (user.password.length < 6) {
+      alert("Password length must be at least 6 characters.");
+      return;
+    }
+
+    registerEmail(user);
   };
 
-  return (
+  return !isAuth ? (
     <div
       className="flex card-white font-ubuntu sm:ml-4 sm:mr-10 mx-16 px-8
       max-w-sm"
@@ -54,7 +85,7 @@ function SignUpCard() {
         <div className="flex">
           <div className="text-3xl font-medium px-7">Get Started</div>
         </div>
-        <form className="flex-col pb-4 pt-7 px-7" onSubmit={handleSubmit}>
+        <form className="flex-col pb-4 pt-7 px-7" onSubmit={(e)=> {handleSubmit(e)}}>
           <div id="signup-form-1" className="relative">
             <input
               placeholder="Email *"
@@ -102,12 +133,14 @@ function SignUpCard() {
           </div>
         </form>
         <div className="px-7 ">
-          <button
+          <button onClick={()=>{
+            registerGoogle();
+          }}
             className="btn-white border border-pink inline-flex 
             justify-center items-center w-full px-7 mb-2"
           >
             <img src={GooglePng} alt="Google SVG" className="h-4 mr-3" />
-            <span>Register with Google</span>
+            <span>Log in with Google</span>
           </button>
           <div className="font-light text-sm">
             <Link to="/portal/login">
@@ -119,6 +152,9 @@ function SignUpCard() {
         </div>
       </div>
     </div>
+  ) : 
+  (
+    navigate("../pages/MainAppPage")
   );
 }
 
