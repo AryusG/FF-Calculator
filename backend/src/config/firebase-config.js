975a1f6cd1e486050fb2b1e42b062f62f47fdc05
AryusG <verdyg24@gmail.com>
2022-07-12 12:54:45 +1000

    added dotenv

--- backend/src/config/firebase-config.js
+++ backend/src/config/firebase-config.js
@@ -1,17 +1,20 @@
-import {initializeApp} from "firebase/app"
-import {getFirestore, collection} from "firebase/firestore"
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../.env" });
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDM-qX2cOyJoVLn8BaqsbhhTtC_fgc0DuQ",
-    authDomain: "ff--calculator.firebaseapp.com",
-    projectId: "ff--calculator",
-    storageBucket: "ff--calculator.appspot.com",
-    messagingSenderId: "609028844257",
-    appId: "1:609028844257:web:0d4c68130f8100591025a0",
-    measurementId: "G-DE2V4J143R"
+  apiKey: process.env.apiKey,
+  authDomain: "ff--calculator.firebaseapp.com",
+  projectId: "ff--calculator",
+  storageBucket: "ff--calculator.appspot.com",
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const user_app_data = collection(db, "user_app_data");
-export {user_app_data, db};
+export { user_app_data, db };
